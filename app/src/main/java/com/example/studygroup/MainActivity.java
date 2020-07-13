1707aa8b8a536fc5f223cf24382cc1105a6b960e
Pablo Garza <pgarza917@fb.com>
2020-07-13 17:58:04 -0400

    Added bottom navigation bar functionality

--- app/src/main/java/com/example/studygroup/MainActivity.java
+++ app/src/main/java/com/example/studygroup/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.studygroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.studygroup.fragments.CreateEventFragment;
+import com.example.studygroup.fragments.FeedFragment;
+import com.example.studygroup.fragments.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    final FragmentManager mFragmentManager = getSupportFragmentManager();
+    private BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBottomNavigationView = findViewById(R.id.bottom_navigation);
+        mBottomNavigationView.setItemIconTintList(null);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch(menuItem.getItemId()) {
+                    case R.id.action_create_event:
+                        fragment = new CreateEventFragment();
+                        break;
+                    case R.id.action_messages:
+                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.action_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    default:
+                        fragment = new FeedFragment();
+                        break;
+                }
+                mFragmentManager.beginTransaction().replace(R.id.frameLayoutContainer, fragment).commit();
+                return true;
+            }
+        });
+
+        // Set default selection as home action icon
+        mBottomNavigationView.setSelectedItemId(R.id.action_home);
     }
 }
