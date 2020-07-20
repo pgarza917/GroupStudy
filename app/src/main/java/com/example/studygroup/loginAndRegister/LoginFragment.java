54583226dee202c84f3dc185cc50f97874435f75
Pablo Garza <pgarza917@fb.com>
2020-07-20 17:26:11 -0400

    Setup project to allow Google sign-in with Google SDK

--- app/src/main/java/com/example/studygroup/loginAndRegister/LoginFragment.java
+++ app/src/main/java/com/example/studygroup/loginAndRegister/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.example.studygroup.loginAndRegister;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -11,12 +12,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.studygroup.MainActivity;
 import com.example.studygroup.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
@@ -27,12 +36,15 @@ import com.parse.ParseUser;
 public class LoginFragment extends Fragment {
 
     public static final String TAG = LoginFragment.class.getSimpleName();
+    public static final int GOOGLE_SIGN_IN_REQUEST_CODE = 5456;
 
+    private GoogleSignInClient mGoogleSignInClient;
 
     private EditText mUsernameEditText;
     private EditText mPasswordEditText;
     private Button mLoginButton;
     private Button mRegisterButton;
+    private SignInButton mGoogleSignInButton;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -49,6 +61,23 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(getContext(), gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getContext());
+
+        if(account != null) {
+            goToMainActivity();
+        }
+
         // Check to see if there is a cached user so we don't make the user have to sign
         // in again if they already have before. Persistence of user login across app
         // restarts
@@ -61,6 +90,7 @@ public class LoginFragment extends Fragment {
         mPasswordEditText = view.findViewById(R.id.passwordEditText);
         mLoginButton = view.findViewById(R.id.loginButton);
         mRegisterButton = view.findViewById(R.id.goToRegisterButton);
+        mGoogleSignInButton = view.findViewById(R.id.googleSignInButton);
 
         // Set click listener to handle registration when 'Register Account' button is tapped
         mLoginButton.setOnClickListener(new View.OnClickListener() {
@@ -73,6 +103,13 @@ public class LoginFragment extends Fragment {
             }
         });
 
+        mGoogleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchGoogleSignIn();
+            }
+        });
+
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,6 +119,30 @@ public class LoginFragment extends Fragment {
                 ((LoginActivity) getContext()).getSupportFragmentManager().beginTransaction().replace(R.id.frameLayoutContainer, fragment).commit();
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode != Activity.RESULT_OK) {
+            Log.i(TAG, "Error on returning to login fragment from launched activity!");
+            return;
+        }
+        if(requestCode == GOOGLE_SIGN_IN_REQUEST_CODE) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                // Signed in successfully, show authenticated UI.
+                goToMainActivity();
+            } catch (ApiException e) {
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            }
+        }
     }
 
     // This method handles the logging in of a user with the user-provided credentials
@@ -107,5 +168,11 @@ public class LoginFragment extends Fragment {
         Intent intent = new Intent(getContext(), MainActivity.class);
         startActivity(intent);
         getActivity().finish();
+    }
+
+    // Method to initiate signing-in with a Google account
+    private void launchGoogleSignIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, GOOGLE_SIGN_IN_REQUEST_CODE);
     }
 }
