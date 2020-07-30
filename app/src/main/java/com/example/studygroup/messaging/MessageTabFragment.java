64fd72db46aa1f99d1ae57f785938af92a86bd91
Pablo Garza <pgarza917@fb.com>
2020-07-29 20:17:36 -0400

    Added logic for displaying chats the user has with other users in DM tab

--- app/src/main/java/com/example/studygroup/messaging/MessageTabFragment.java
+++ app/src/main/java/com/example/studygroup/messaging/MessageTabFragment.java
@@ -1,21 +1,34 @@
 package com.example.studygroup.messaging;
 
+import android.animation.FloatEvaluator;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.SearchView;
 
 import com.example.studygroup.R;
+import com.example.studygroup.adapters.FirebaseUserAdapter;
+import com.example.studygroup.adapters.UsersAdapter;
+import com.example.studygroup.models.Message;
 import com.example.studygroup.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,6 +43,10 @@ public class MessageTabFragment extends Fragment {
 
     private int mTabPosition;
     private List<User> mUsersList;
+    private List<String> mUserIdList;
+    private FirebaseUserAdapter mFirebaseUsersAdapter;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabaseReference;
 
     public MessageTabFragment() {
         // Required empty public constructor
@@ -57,19 +74,79 @@ public class MessageTabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message_tab, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_message_tab, container, false);
 
         mMessagesRecyclerView = view.findViewById(R.id.messagesListRecyclerView);
+        mMessagesRecyclerView.setHasFixedSize(true);
+        mMessagesRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        if(mTabPosition == 1) {
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        } else {
+        mUserIdList = new ArrayList<>();
 
-        }
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("Chats");
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUserIdList.clear();
+
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Message message = snapshot.getValue(Message.class);
+
+                    if(message.getSender().equals(mFirebaseUser.getUid())) {
+                        mUserIdList.add(message.getReceiver());
+                    }
+                    if(message.getReceiver().equals(mFirebaseUser.getUid())) {
+                        mUserIdList.add(message.getSender());
+                    }
+                }
+
+                readChats();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "Error reading Firebase for chats: ", error.toException());
+            }
+        });
+
+        return view;
+    }
+
+    private void readChats() {
+        mUsersList = new ArrayList<>();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("Users");
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mUsersList.clear();
+
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    User user = snapshot.getValue(User.class);
+
+                    for(String id : mUserIdList) {
+                        if(user.getId().equals(id)) {
+                            if(!mUsersList.isEmpty()) {
+                                for(User user1 : mUsersList) {
+                                    if(!user.getId().equals(user1.getId())) {
+                                        mUsersList.add(user);
+                                    }
+                                }
+                            } else {
+                                mUsersList.add(user);
+                            }
+                        }
+                    }
+                }
+
+                mFirebaseUsersAdapter = new FirebaseUserAdapter(getContext(), mUsersList);
+                mMessagesRecyclerView.setAdapter(mFirebaseUsersAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e(TAG, "Error reading users from Firebase: ", error.toException());
+            }
+        });
     }
 }
